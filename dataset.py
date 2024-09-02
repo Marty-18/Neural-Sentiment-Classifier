59eafa116b3012d2ba226bc0e43388c54df818e8
Martina Emmerich <martinaemmerich@Martinas-Air.station>
2024-09-02 16:13:42 +0200

    added dataset class and vocabulary class

--- dataset.py
+++ dataset.py
@@ -1,8 +1,34 @@
 import pandas as pd
 import numpy as np
+import spacy
+from torch.utils.data import Dataset
+
+class DataVocab:
+    """
+    Create vocabulary to encode the whole dataset based on the training data to avoid data leaks. 
+    """
+    def __init__(self, train_data, vocab_size=10000, max_len=267):
+         self.vocab = create_vocab(count_vocab(train_data['review']))
+        
+        
+        def count_vocab:
+        
+        def create_vocab:
+        
 
 
-class SentiData:
-    def __init__(self, file, tokenizer, vocab_size=10000, max_len=267):
-        self.data = pd.load_csv(file)
+class ClassifierTextDataset(Dataset):
+    def __init__(self, data, tokenizer, vocab): # vocab_size=10000, max_len=267):
+        self.data = data
+        #self.vocab_size = vocab_size
         self.tokenizer = tokenizer
+        self.vocab = vocab
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        return self.encodings[idx], self.labels[idx]
+    
+    def pad_collare():
+       
