ca8dcd6d59917c3b1b75756b79f851a787b00da2
Marty-18 <martina.emmerich@gmail.com>
2024-09-16 11:41:49 +0200

    Add eval epoch method

--- trainer.py
+++ trainer.py
@@ -100,8 +100,60 @@ class Trainer():
 
         return average_train_loss , train_f1_score, final_elapsed
 
-    def eval_epoch(self):
-        pass
+    def eval_epoch(self, epoch):
+        # model in evaluation mode
+        self.model.eval()
+        total_val_loss = 0
+        total_val_samples = 0
+        t0 = time.time()
+        all_labels = []
+        all_preds = []
+       
+        # display progress
+        progress_bar_eval = tqdm(self.val_data, desc=f'| Epoch validation {epoch}', leave=True, disable=False)
+        
+        with torch.no_grad():
+            for step, batch in enumerate(progress_bar_eval):
+                inputs = batch[0][0]
+                labels = batch[0][1]
+                # print(inputs.shape)
+      
+                # get model predictions
+                outputs = self.model(inputs)
+
+                pred = torch.round(outputs)
+                val_loss = self.criterion(outputs, labels.float())
+
+                total_val_loss += val_loss.item() * len(inputs)
+                total_val_samples += len(inputs)
+
+                all_labels.append(labels)
+                all_preds.append(pred.int())
+          
+        all_labels = torch.cat(all_labels)
+        all_preds = torch.cat(all_preds)
+    
+
+        # accuracy
+        val_acc = (torch.sum(all_labels == all_preds))/total_val_samples
+        print(f'Val accuracy: {val_acc:.4f}')
+  
+        # f1 score
+        val_f1_score = binary_f1_score(all_labels, all_preds, average='weighted') #y_true, y_pred
+ 
+        # validation loss
+        average_val_loss =  total_val_loss/total_val_samples
+        # validation time
+        final_elapsed = format_time(time.time()-t0)
+
+
+        # update the stats dictionary
+        self.stats_dict['val_acc'].append(val_acc)
+        self.stats_dict['val_f1_score'].append(val_f1_score)
+        self.stats_dict['val_time'].append(final_elapsed)
+        self.stats_dict['val_loss'].append(average_val_loss)
+
+        return average_val_loss, val_f1_score, final_elapsed
 
     def save_model(self):
         "not implemented"
@@ -117,14 +169,25 @@ class Trainer():
         for epoch in range(self.num_epochs):
             epoch_t0 = time.time()
             epoch_train_loss, train_f1_score, train_time = self.train_epoch(epoch+1)
-           #### avg_val_loss, f1_val_score, val_time = evaluate(val_dataloader, epoch+1)
+            avg_val_loss, f1_val_score, val_time = self.eval_epoch(epoch+1)
             self.scheduler.step()
             epoch_time = time.time()-epoch_t0
-            print(f"End of epoch {epoch+1}/{self.num_epochs}, Train Loss: {epoch_train_loss:.4f}, Train F1 Score: {train_f1_score}, Learning rate scheduler: {self.optimizer.param_groups[0]['lr']}.")
-            print(f"Epoch time: {format_time(epoch_time)}, Train time: {train_time}.")
-            #print(f"End of epoch {epoch+1}/{num_epochs}, Train Loss: {epoch_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Train F1 Score: {train_f1_score}, Val F1 Score: {f1_val_score:.4f}, Learning rate scheduler: {optimizer.param_groups[0]['lr']}.")
-            #print(f"Epoch time: {format_time(epoch_time)}, Train time: {train_time}, Validation time: {val_time}.")
+            print(f"End of epoch {epoch+1}/{self.num_epochs}, Train Loss: {epoch_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Train F1 Score: {train_f1_score}, Val F1 Score: {f1_val_score:.4f}, Learning rate scheduler: {self.optimizer.param_groups[0]['lr']}.")
+            print(f"Epoch time: {format_time(epoch_time)}, Train time: {train_time}, Validation time: {val_time}.")
+            total_elapsed_train += epoch_time
+            # early stopping
+            if f1_val_score > best_f1_val:
+                best_f1_val = f1_val_score
+                bad_epochs = 0
+            else:
+                bad_epochs += 1
+                print('BAD EPOCH!')
+               # print(bad_epochs)
+            if bad_epochs >= self.patience:
+                print(f'No validation set improvements observed for {bad_epochs} epochs. Stopping training early!')
+                break
 
-
+        print('FINISHED TRAINING SUCCESSFULLY!')
+        print(f'Total time took for training: {format_time(total_elapsed_train)}')
 
     
